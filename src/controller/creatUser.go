3f31b8a52c26f7e40303631d6b216d014162ff03
Jhonatan Gonçalves <play4fungameplay@gmail.com>
2025-01-13 16:49:38 -0300

    Criado objetos de usuário e a validação dos campos de requisição.

--- src/controller/creatUser.go
+++ src/controller/creatUser.go
@@ -1,12 +1,25 @@
 package controller
 
 import (
-	"github.com/ForFun21/crud-go/src/configuration/rest_err"
+	"fmt"
+	"log"
+
+	"github.com/ForFun21/crud-go/src/configuration/rest_err/validation"
+	"github.com/ForFun21/crud-go/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreatUser(c *gin.Context) {
+	log.Println("Init CreateUser controller")
+	var userRequest request.UserRequest
 
-	err := rest_err.NewBadRequestError("Voce chamou a rota de forma errada.")
-	c.JSON(err.Code, err)
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		log.Printf("Error trying to marshal object, error=%s\n", err.Error())
+		errRest := validation.ValidateUserError(err)
+
+		c.JSON(errRest.Code, errRest)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
